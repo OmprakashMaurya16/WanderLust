14686ce558dc3a1d5bbdce72a6eaa3333fbcfb1c
Omprakash Maurya <omprakash.maurya1604@gmail.com>
2025-06-23 21:13:09 +0530

    Async

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
@@ -8,11 +10,8 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 
 const app = express();
-const PORT = 3000;
-const MONGO_URL =
-  "mongodb+srv://omprakashmaurya1604:Maurya2005@wanderlust.dw8uq0b.mongodb.net/WanderLust";
-
-// Set view engine and middleware
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.engine("ejs", ejsMate);
@@ -20,23 +19,25 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-// MongoDB connection
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
 main()
   .then(() => console.log("DB Connected Successfully"))
   .catch((err) => console.log(`Database Connection Error: ${err}`));
 
-// Routes
 app.get("/", (req, res) => {
   res.send("Welcome to WanderLust");
 });
 
-app.get("/listing", async (req, res) => {
-  const allListing = await Listing.find({});
-  res.render("listings/index", { allListing });
-});
+app.get(
+  "/listing",
+  wrapAsync(async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("listings/index", { allListing });
+  })
+);
 
 app.get("/listing/new", (req, res) => {
   res.render("listings/new");
@@ -51,42 +52,51 @@ app.post(
   })
 );
 
-app.get("/listing/:id", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show", { listing });
-});
+app.get(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show", { listing });
+  })
+);
 
-app.get("/listing/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit", { listing });
-});
+app.get(
+  "/listing/:id/edit",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
+  })
+);
 
-app.put("/listing/:id", async (req, res) => {
-  const { id } = req.params;
-  await Listing.findByIdAndUpdate(id, req.body.listing);
-  res.redirect(`/listing/${id}`);
-});
+app.put(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, req.body.listing);
+    res.redirect(`/listing/${id}`);
+  })
+);
 
-app.delete("/listing/:id", async (req, res) => {
-  const { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listing");
-});
+app.delete(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listing");
+  })
+);
 
-// Catch-all 404 handler
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
 });
 
-// Central error handler
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong!" } = err;
   res.status(statusCode).send(message);
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
