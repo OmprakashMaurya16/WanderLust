c31280fd79bb07cf4ea3da325b36452f0effe29a
Omprakash Maurya <omprakash.maurya1604@gmail.com>
2025-06-30 22:40:19 +0530

    Added All project file

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ async function main() {
 const store = MongoStore.create({
   mongoUrl: MONGO_URL,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -48,7 +48,7 @@ store.on("error", () => {
 
 const sessionOption = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
